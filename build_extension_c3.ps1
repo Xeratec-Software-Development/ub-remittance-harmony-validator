b89e0c4398744fc4c2cf90dfac10848a097a9149
casiala <carl.asiala@xeratec.com>
2025-02-06 10:27:37 -0500

    Add PowerShell script for deploying files to remote Harmony service

---
+++ build_extension_c3.ps1
@@ -0,0 +1,36 @@
+# Define variables
+$REMOTE_USER = "harmony_admin"
+$REMOTE_HOST = "4.227.177.52"
+$SSH_PEM = ".\harmony_testcorner3.pem"
+$REMOTE_DIR = "/etc/harmony-ap/extensions/lib/"
+$FileToCopy = $args[0]
+
+# Maven installation step
+# Write-Output "==============================="
+# Write-Output "Running Maven Install..."
+# Write-Output "==============================="
+# & mvn install -Ptomcat -Pdefault-plugins -Pdatabase -PUI "-Dmaven.test.skip=true"
+
+# Stop the harmony service on the remote host
+# Write-Output "==============================="
+# Write-Output "Stopping harmony..."
+# Write-Output "==============================="
+# ssh -i ${SSH_PEM} "${REMOTE_USER}@${REMOTE_HOST}" "sudo systemctl stop harmony-ap"
+
+# Copy file to remote directory
+Write-Output "==============================="
+Write-Output "Copying file to remote..."
+Write-Output "==============================="
+scp -i ${SSH_PEM} ${FileToCopy} "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
+
+# Start the harmony service on the remote host
+# Write-Output "==============================="
+# Write-Output "Starting harmony..."
+# Write-Output "==============================="
+# ssh -i ${SSH_PEM} "${REMOTE_USER}@${REMOTE_HOST}" "sudo systemctl start harmony-ap"
+
+# Restart the harmony service on the remote host
+Write-Output "==============================="
+Write-Output "Restarting harmony..."
+Write-Output "==============================="
+ssh -i ${SSH_PEM} "${REMOTE_USER}@${REMOTE_HOST}" "sudo systemctl restart harmony-ap"
